3c65b635c3c08573fcae9cfc178f393948822b00
chaeju <cjlim000523@gmail.com>
2024-12-24 18:39:53 +0900

    Save local changes before merging

--- gitproject/src/main/java/A.java
+++ gitproject/src/main/java/A.java
@@ -1,6 +1,6 @@
 public class A {
     public static void main(String[] args) {
-        System.out.println("class A update for brach B");
+        System.out.println("class A update for brach devC");
     }
 }
 
--- gitproject/src/main/java/C.java
+++ gitproject/src/main/java/C.java
@@ -1,4 +1,5 @@
 public class C {
     public static void main(String[] args) {
+        System.out.println("class C update for brach devC");
     }
 }
